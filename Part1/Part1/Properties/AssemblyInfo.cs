1554d987810e41a6740b86bb2a9ea49ae59aa02f
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-02-21 08:16:16 -0500

    Created Part1

---
+++ Part1/Part1/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Part1")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Part1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
