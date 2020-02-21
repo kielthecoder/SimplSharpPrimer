ada1673d429f69d6c47ff9cd35697ef227165355
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-02-21 14:13:52 -0500

    Added a touchpanel

--- Primer/Part3/ControlSystem.cs
+++ Primer/Part3/ControlSystem.cs
@@ -2,11 +2,15 @@ using System;
 using Crestron.SimplSharp;
 using Crestron.SimplSharpPro;
 using Crestron.SimplSharpPro.CrestronThread;
+using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharpPro.UI;
 
 namespace Part3
 {
     public class ControlSystem : CrestronControlSystem
     {
+        private Tsw1050 _tp;
+
         public ControlSystem()
             : base()
         {
@@ -15,7 +19,14 @@ namespace Part3
 
         public override void InitializeSystem()
         {
+            _tp = new Tsw1050(0x03, this);
+            _tp.SigChange += new SigEventHandler(_tp_SigChange);
+            _tp.Register();
+        }
 
+        void _tp_SigChange(BasicTriList currentDevice, SigEventArgs args)
+        {
+            throw new NotImplementedException();
         }
    }
 }
