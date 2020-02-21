6c0350bf1bf9d40d5dfb2e3fdc4b6eac0f693596
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-02-21 09:08:53 -0500

    Created Part3 project

---
+++ Primer/Part3/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Part3")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Part3")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
