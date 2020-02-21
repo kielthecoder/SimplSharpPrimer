8a9970f8ba6bfe155dcf91e99b0957d0664c3b13
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-02-21 17:29:46 -0500

    Update ControlSystem.cs

--- Primer/Part3/ControlSystem.cs
+++ Primer/Part3/ControlSystem.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections.Generic;
 using Crestron.SimplSharp;
 using Crestron.SimplSharpPro;
 using Crestron.SimplSharpPro.CrestronThread;
 using Crestron.SimplSharpPro.DeviceSupport;
 using Crestron.SimplSharpPro.DM;
+using Crestron.SimplSharpPro.DM.Cards;
 using Crestron.SimplSharpPro.DM.Endpoints.Receivers;
 using Crestron.SimplSharpPro.DM.Endpoints.Transmitters;
 using Crestron.SimplSharpPro.UI;
@@ -14,6 +16,8 @@ namespace Part3
     {
         private Tsw1050 _tp;
         private Switch _sw;
+        private List<CardDevice> _inputs;
+        private List<DmcOutputSingle> _outputs;
 
         private DmTx4K100C1G _tx;
         private DmRmc4k100C _rx;
@@ -30,37 +34,41 @@ namespace Part3
             _tp.SigChange += new SigEventHandler(_tp_SigChange);
             _tp.Register();
 
+            _inputs = new List<CardDevice>();
+            _outputs = new List<DmcOutputSingle>();
+
             _sw = new DmMd8x8(0x10, this);
+            _inputs.Add(new Dmc4kC(1, _sw));
+            _outputs.Add(new DmcCoHdSingle(1, _sw));
+            
+            _tx = new DmTx4K100C1G(0x14, _sw.Inputs[1]);
+            _tx.OnlineStatusChange +=new OnlineStatusChangeEventHandler(_tx_OnlineStatusChange);
+            
+            _rx = new DmRmc4k100C(0x15, _sw.Outputs[1]);
+            _rx.OnlineStatusChange += new OnlineStatusChangeEventHandler(_rx_OnlineStatusChange);
+            
             _sw.OnlineStatusChange += new OnlineStatusChangeEventHandler(_sw_OnlineStatusChange);
             _sw.Register();
-
-            _tx = new DmTx4K100C1G(_sw.Inputs[1]);
-            _tx.OnlineStatusChange += new OnlineStatusChangeEventHandler(_tx_OnlineStatusChange);
-            _tx.Register();
-
-            _rx = new DmRmc4k100C(_sw.Outputs[1]);
-            _rx.OnlineStatusChange += new OnlineStatusChangeEventHandler(_rx_OnlineStatusChange);
-            _rx.Register();
         }
 
         void _tp_SigChange(BasicTriList currentDevice, SigEventArgs args)
         {
-            throw new NotImplementedException();
+
         }
 
         void _sw_OnlineStatusChange(GenericBase currentDevice, OnlineOfflineEventArgs args)
         {
-            throw new NotImplementedException();
+
         }
 
         void _tx_OnlineStatusChange(GenericBase currentDevice, OnlineOfflineEventArgs args)
         {
-            throw new NotImplementedException();
+
         }
 
         void _rx_OnlineStatusChange(GenericBase currentDevice, OnlineOfflineEventArgs args)
         {
-            throw new NotImplementedException();
+
         }
     }
 }
