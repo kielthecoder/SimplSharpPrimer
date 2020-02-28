d0a8e013cecfec4dbf4c139606ae072363819757
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-02-28 11:18:09 -0500

    Created Part4 project

---
+++ Primer/Part4/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Part4")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Part4")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
