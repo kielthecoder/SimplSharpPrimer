f7ba754150f1f3ff9cd3bdbf735208134b675121
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-03-04 13:33:52 -0500

    Started Part5

---
+++ Primer/Part5/ControlSystem.cs
@@ -0,0 +1,37 @@
+using System;
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.CrestronThread;
+using Crestron.SimplSharpPro.Diagnostics;
+using Crestron.SimplSharpPro.DeviceSupport;
+
+namespace Part5
+{
+    public class ControlSystem : CrestronControlSystem
+    {
+        public ControlSystem()
+            : base()
+        {
+            try
+            {
+                Thread.MaxNumberOfUserThreads = 20;
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in ControlSystem constructor: {0}", e.StackTrace);
+            }
+        }
+
+        public override void InitializeSystem()
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error in InitializeSystem: {0}", e.StackTrace);
+            }
+        }
+    }
+}
---
+++ Primer/Part5/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Part5")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Part5")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
