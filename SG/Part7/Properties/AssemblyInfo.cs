f3200e1f441f254067ed8b709b989aa8ba31cc12
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-11-19 11:46:22 -0500

    first checkin of part 7

---
+++ SG/Part7/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Part7")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Part7")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
